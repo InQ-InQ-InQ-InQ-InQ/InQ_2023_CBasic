a87250e6f13f56350ab25f7a4935235c5b7c2411
ham0609 <kee2621@naver.com>
2023-04-07 18:56:57 +0900

    [1주차] 함준혁 과제 제출

---
+++ 함준혁/Week 1/week1 10869.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+
+{
+	int a, b;
+
+	
+	scanf(" %d %d",&a,&b);
+
+
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+
+}
