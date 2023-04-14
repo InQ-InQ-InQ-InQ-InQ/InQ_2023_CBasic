c87d3988e817ad0c056fd23f653aa684c078d2cb
ham0609 <128105167+ham0609@users.noreply.github.com>
2023-04-14 10:00:31 +0900

    [2주차] 함준혁 과제 제출 (#9)
    * [1주차] 함준혁 과제 제출
    
    * [2주차] 함준혁 과제 제출

---
+++ 함준혁/Week 2/week2_2884.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int h, m;
+    scanf("%d %d", &h, &m);
+
+    m -= 45;
+    if (m < 0)
+    {
+        m += 60;
+        h -= 1;
+
+        if (h < 0) h = 23;
+    }
+
+    printf("%d %d", h, m);
+
+    return 0;
+}
---
+++ 함준혁/Week 2/week2_9498.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+    int score;
+
+    scanf("%d", &score);
+
+    if (score >= 90)
+        printf("A\n");
+    else if (score >= 80)
+        printf("B\n");
+    else if (score >= 70)
+        printf("C\n");
+    else if (score >= 60)
+        printf("D\n");
+    else
+        printf("F\n");
+
+    return 0;
+
+
+
+}
+
+
