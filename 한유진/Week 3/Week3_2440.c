d4f74579d4c472671fc93c3cd4a004315f40d9ba
cho4u4o <cho4u4o@gmail.com>
2023-05-04 13:50:58 +0900

    [3주차] 한유진 과제 제출

---
+++ 한유진/Week 3/Week3_2440.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j, n;
+
+    scanf("%d", &n);
+
+    for (i = n; i > 0; i--)
+    {
+        for (j = i; j > 0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
