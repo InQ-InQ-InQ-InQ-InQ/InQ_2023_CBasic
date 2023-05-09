29cacea099e9de5406c23cbe964cfa39c1dfc90b
cho4u4o <128016678+cho4u4o@users.noreply.github.com>
2023-05-09 16:26:15 +0900

    Update inq_atoi.c
    수정했습니다! overflow, underflow 처리 로직을 지우니까 정상작동하여 일단 지웠습니다!

--- 한유진/Week 4/inq_atoi.c
+++ 한유진/Week 4/inq_atoi.c
@@ -1,20 +1,19 @@
 #define INT_MAX 2147483647
 #define INT_MIN (-INT_MAX - 1)
 
-int let_me_know(char c) { //문자열이 숫자인지 아닌지를 판단하는 함수
-    return c >= '0' && c <= '9';
+int is_digit (char c) { 
+return c >= '0' && c <= '9';
 }
 
-int inq_atoi(char* str) { // 코드의 목적 - char에서 숫자만 뽑아서 return
-    int sign = 1; // 부호를 저장할 변수, 나중에 num과 곱해서 return
-    int num = 0; // 변환된 숫자를 저장할 변수
+int inq_atoi(char *str){ 
+    int sign = 1;
+    int num = 0; 
 
     // 공백 제거
-    while (*str == ' ') { //*str이 공백일 때, str++하기
+    while (*str == ' ') { 
         str++;
     }
 
-    // 부호 처리, 비정상적인 문자열 입력시 0으로 반환
     if (*str == '-') {
         sign = -1;
         str++;
@@ -23,20 +22,11 @@ int inq_atoi(char* str) { // 코드의 목적 - char에서 숫자만 뽑아서 r
         str++;
     }
 
-    // 숫자 처리, num 도출
-    while (let_me_know(*str)) {
+    // 숫자 처리
+    while (is_digit (*str)) {
         num = num * 10 + (*str - '0');
-        str++;  // 이미 여기서 터져버리는것..!!
+        str++;
     }
 
-    //overflow, underflow solution
-    if (sign > 0 && num >= INT_MIN ) {
-        return INT_MAX;
-        }
-    else if (sign < 0 && num <= INT_MAX ) {
-        return INT_MIN;
-        }
-    else
-        return num * sign; 
-   
+    return num * sign;
 }
